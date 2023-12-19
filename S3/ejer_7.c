a5503d4885afb6e75aafceef58655d5b23d7cca5
LeviDooku <pedrovelascosantana@gmail.com>
2023-12-19 11:56:07 +0100

    Añadido nuevo ejercicio S3

--- S3/ejer_7.c
+++ S3/ejer_7.c
@@ -22,6 +22,7 @@ background en caso contrario. Si el programa tiene argumentos hay que ejecutarlo
 
 int main(int argc, char *argv[]){
 
+    //Filtro del número de argumentos
     if(argc < 2){
         printf("[-] Número de argumentos incorrecto\n");
         printf("USO: %s <programa> [<arg[1]...arg[n]>] [bg]\n", argv[0]);
@@ -36,19 +37,19 @@ int main(int argc, char *argv[]){
 	    exit(EXIT_FAILURE);
     }
     else if(pid == 0){
-        if(argc > 2 && strcmp(argv[argc - 1], "bg") == 0){
+        if(argc > 2 && strcmp(argv[argc - 1], "bg") == 0){ //Modificamos la cadena para eliminar "bg", para que no sea detectado como argumento
             argv[argc - 1] = NULL;
             execvp(argv[1], &argv[1]);
         }
         else{
-            if(execvp(argv[1], &argv[1]) != 0){
+            if(execvp(argv[1], &argv[1]) != 0){ //Ejecutamos con execvp. Es la más apropiada, busca el ejecutable en el path como no se sabe el num. args. es más cómodo
                 perror("[-] ERROR EN EXECVP");
                 exit(EXIT_FAILURE);
             }
         }
     }
     else{
-        if(argc == 2 ||(argc > 2 && strcmp(argv[argc-1], "bg") != 0))
+        if(argc == 2 ||(argc > 2 && strcmp(argv[argc-1], "bg") != 0)) //Si se requiere que se ejecute en foreground, se espera a que el hijo termine
             wait(NULL);
     }
 }
